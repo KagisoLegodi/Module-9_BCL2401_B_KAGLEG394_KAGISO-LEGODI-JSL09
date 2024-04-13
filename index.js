11cb3ae76ce6d503949bc132b9769e07020d1226
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-04-13 04:13:27 +0200

    Change query parameter to use real image URL

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=;hjksdf;kljsdfgl;kdsjfgljksdfglkjhsdfg")
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
         console.log(data)
