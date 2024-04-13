99236e07cf496f91350f034afc09411090e72ec6
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-04-13 04:22:13 +0200

    Handle errors and use default background image

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=sdfghmxfghmxcgh")
     .then(res => res.json())
     .then(data => {
        console.log(data.urls.regular)
@@ -9,5 +9,7 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         console.log("Something went wrong! 😭")
         // This is where I can handle the error
         // Choose to use a default background image
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1535961652354-923cb08225a7?crop=entropy&amp;cs=tinysrgb&amp;fit=max&amp;fm=jpg&amp;ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTI5NzQ1MzB8&amp;ixlib=rb-4.0.3&amp;q=80&amp;w=1080&quot;)`
+
     })
 
