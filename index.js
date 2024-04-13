bb6a8c128461afbeb82661737e2f55e4413df37b
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-04-13 04:14:19 +0200

    Log the image URL to the console

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
-        console.log(data)
-        throw Error("I'm an error!")
-        // document.body.style.backgroundImage = `url(${data.urls.regular})`
-		// document.getElementById("author").textContent = `By: ${data.user.name}`
+       console.log(data.urls.regular)
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+		document.getElementById("author").textContent = `By: ${data.user.name}`
     })
     .catch(err => {
         console.log("Something went wrong! 😭")
