a3669bcce8348aa1391a236e855d7d36bc7e8f4a
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-04-13 04:30:44 +0200

    Set default background image and author name

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
 		document.getElementById("author").textContent = `By: ${data.user.name}`
     })
     .catch(err => {
-        console.log("Something went wrong! 😭")
-        // This is where I can handle the error
-        // Choose to use a default background image
+         // Use a default background image/author
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1535961652354-923cb08225a7?crop=entropy&amp;cs=tinysrgb&amp;fit=max&amp;fm=jpg&amp;ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTI5NzQ1MzB8&amp;ixlib=rb-4.0.3&amp;q=80&amp;w=1080&quot;)`
-
+        document.getElementById("author").textContent = `By: Dodi Achmad`
     })
 
